808a795e74f698dc45ef8c2d3113065ee30ee680
salyviah <salyviahkamau@gmail.com>
2024-03-22 21:45:36 +0300

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+    let cats = ["Milo", "Otis", "Garfield"]
+    const allCats = [...cats]; 
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield")
+  
+    function destructivelyAppendCat (Browny) {
+        cats.push(Browny)
+        console.log(cats)
+    }
+
+    function destructivelyPrependCat(Browny) {
+        cats.unshift(Browny)
+        console.log(cats)
+    }
+l
+    function destructivelyRemoveLastCat() {
+        cats.pop()
+        console.log(cats)
+    }
+
+    function destructivelyRemoveFirstCat() {
+        cats.shift()
+        console.log(cats)
+    }
+
+    function appendCat(cat) {
+        return cats.concat(cat);
+    }
+    
+    function prependCat(cat) {
+        return [cat, ...cats];
+    }
+    
+    function removeLastCat() {
+        return cats.slice(0, -1);
+    }
+
+
+    function removeFirstCat() {
+    return cats.slice(1);
+}
+    
+
+    
+    
